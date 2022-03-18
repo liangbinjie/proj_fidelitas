feb455122d03d63e29d2ea0f76d512fe600a719f
liangbinjie <67171031+liangbinjie@users.noreply.github.com>
2022-03-17 23:54:36 -0600

    Update buscador.py

--- programacion basica/buscador.py
+++ programacion basica/buscador.py
@@ -1,25 +1,36 @@
 # Modulo para hacer busqueda dentro del inventario
 
+productos_path = "programacion basica/proyecto/producto.txt"
 
 # Funcion para hacer una busqueda general
 def bgeneral(producto):
     # vamos a abrir el archivo plano donde estan almacenados los productos
-    with open("producto.txt") as productos:
+    with open(productos_path) as productos:
         for linea in productos: # por cada linea que tiene el archivo plano
-            datos = linea.split() # dividiremos los datos que tiene cada linea
-            if producto in datos: # si el nombre del producto esta dentro de la linea
+            datos = linea.split(",") # dividiremos los datos que tiene cada linea
+            # encontrado = 0
+            if producto in datos[0] or producto in datos[1]: # si el nombre del producto esta dentro de la linea
+                # encontrado = 1
                 return f"\nNombre de producto: {datos[0]}\nCodigo: {datos[1]}\nPrecio: {datos[2]}\nEn inventario: {datos[3]}"  # regresamos el nombre, codigo y precio del producto
 
+            # elif encontrado == 0:
+            #     return "Producto no encontrado"
 
 
 # Funcion para hacer una busqueda por medio de codigo
 def bcodigo(codigo):
     # vamos a abrir el archivo plano donde estan almacenados los productos
-    with open("producto.txt") as productos:
+    with open(productos_path) as productos:
         for linea in productos: # por cada linea que tiene el archivo plano
-            datos = linea.split() # vamos a dividir los datos de cada linea dentro de un arreglo
-            if codigo in datos: # si el codigo del producto esta dentro de la linea
-                return "Nombre producto: " + datos[0], "Precio: " + datos[2], "Cant Disponible: " + datos[3] # regresamos el nombre y precio del producto
+            datos = linea.split(",") # vamos a dividir los datos de cada linea dentro de un arreglo
+            encontrado = 0
+            if codigo in datos[1]: # si el codigo del producto esta dentro de la linea
+                encontrado = 1
+                return f"Nombre producto: {datos[0]}\nPrecio: {datos[2]}\nCantidad Disponible: {datos[3]}" # regresamos el nombre y precio del producto
+            
+            elif encontrado == 0:
+                return "Producto no encontrado"
+            
 
 
 
@@ -27,12 +38,13 @@ def bcodigo(codigo):
 def brango(rango_i:int, rango_f:int):
     lista = []
     # rango_i seria precio inicial y rango_f seria precio final, para marcar el rango de los precios
-    with open("producto.txt") as productos:
+    with open(productos_path) as productos:
         for linea in productos: # por cada linea que hay en producto.txt
-            datos = linea.split() # dividimos los datos de cada linea
+            datos = linea.split(",") # dividimos los datos de cada linea
             if int(datos[2]) >= rango_i and int(datos[2]) <= rango_f: # si los datos del precio es igual o mayor al rango inicial y menior o igual al rango final
 
                 lista.append(f"{datos[0]}: {datos[2]}") # agregamos el nombre del producto y su precio dentro del arreglo llamado lista
                 
-
+    print("Hay un total de", len(lista), "productos con ese rango de precio")
     return lista # al final regresamos el arreglo con todos los datos
+
