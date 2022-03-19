00f94f10684dad4b1c7971b889be08db6b8f7d24
liangbinjie <67171031+liangbinjie@users.noreply.github.com>
2022-03-18 18:29:22 -0600

    Update main.py

--- programacion basica/main.py
+++ programacion basica/main.py
@@ -5,25 +5,25 @@ from modificador import *
 running = True
 
 while running:
-    print("Que desea hacer?\n[1]Buscar\n[2]Agregar\n[3]Eliminar\n[0]Salir")
-    do = input("> ")
+    print("Que desea hacer?\n[1]Buscar\n[2]Agregar\n[3]Eliminar\n[4]Modificar\n[0]Salir")
+    do = input("> ").lower()
 
-    if do.lower() == "buscar" or do == "1":
+    if do == "buscar" or do == "1":
         print('- - - - - - - - - - - - - - - - - - - -')
-        tipo = input("Tipo de busqueda\n-General\n-Codigo\n-Precio\n\n >").lower()
+        tipo = input("Tipo de busqueda\n[1]General\n[2]Precio\n\n > ").lower()
         print('- - - - - - - - - - - - - - - - - - - -')
 
-        if tipo == "general":
+        if tipo == "general" or tipo == "1":
             nombre_producto = input("Ingrese nombre de producto o codigo: ").lower()
             print(bgeneral(nombre_producto))
 
 
-        elif tipo == "codigo":
-            codigo = input("Ingrese codigo de producto: ")
-            print(bcodigo(codigo))
+        # elif tipo == "codigo" or tipo == "2":
+        #     codigo = input("Ingrese codigo de producto: ")
+        #     print(bcodigo(codigo))
 
 
-        elif tipo == "precio":
+        elif tipo == "precio" or tipo == "2":
             rango_i = int(input("Ingrese precio inicial: "))
             rango_f = int(input("Ingrese precio de rango final: "))
 
@@ -36,7 +36,7 @@ while running:
 
 
 
-    elif do.lower() == "agregar" or do == "2":
+    elif do == "agregar" or do == "2":
         print('- - - - - - - - - - - - - - - - - - - -')
         nombre_producto = input("Nombre de producto a agregar: ")
         codigo = input("Ingrese codigo de producto: ")
@@ -44,9 +44,17 @@ while running:
         stock = int(input("Ingrese cantidad de productos en inventario: "))
         agregar(nombre_producto.lower(), codigo, precio, stock)
 
+    
+    elif do == "modificar" or do == "4":
+        print('- - - - - - - - - - - - - - - - - - - -')
+        producto = input("Nombre o codigo del producto: ")
+        dato = input("Tipo de dato: ")
+        nuevo = input("Dato nuevo: ")
+        modificar(producto, dato, nuevo)
 
 
-    elif do.lower() == "eliminar" or do == "3":
+
+    elif do == "eliminar" or do == "3":
         print('- - - - - - - - - - - - - - - - - - - -')
         producto = input("Ingrese nombre o codigo de producto: ")
         eliminar(producto)
