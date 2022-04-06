8a1e02e0a7111faefac27402824951fc933c6e2a
liangbinjie <67171031+liangbinjie@users.noreply.github.com>
2022-04-06 13:27:28 -0600

    Update main.py

--- programacion basica/main.py
+++ programacion basica/main.py
@@ -1,9 +1,8 @@
-from inventario import inventario
 from comprar import *
 from os import system
-from getpass import getpass
 from reclamos import *
 from cliente import cliente
+from admin import *
 
 system('cls')
 basedatos = {"admin": "admin1234", "benji": "1386"}
@@ -22,36 +21,8 @@ while running:
         cliente(respuesta)
 
 
-
     if respuesta == "2":
-        usuario = input("Usuario: ")
-        contra = getpass("Contraseña: ")
-
-
-        while usuario not in basedatos or contra != basedatos[usuario]:
-            intentos += 1
-            print("Invalido, intente otra vez")
-            usuario = input("Usuario: ")
-            contra = getpass("Contraseña: ")
-            if intentos == 3:
-                print("Has usado el numero maximo de intentos")
-                quit()
-        
-        else:
-            system("cls")
-            print("Adonde quieres entrar?\n[1]Inventario\n[2]Reportes\n[3]Ver Reclamos")
-            admin = input("> ")
-
-            if admin == "1":
-                system("cls")
-                print('\n- - - - - - - - - - - - - - - - - - - -')
-                print("Has entrado al inventario")
-                inventario()
-            # Creacion de funcion para Reportes generales
-
-            if admin == "3":
-                verReclamos()
-
+        admin()
 
     if respuesta == "3":
         running = False
